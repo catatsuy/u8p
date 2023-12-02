13874961864bfa75d6c3647f405544456ab6ae7f
catatsuy <catatsuy@catatsuy.org>
2023-12-02 19:01:18 +0900

    Refactor code to improve readability

--- example_test.go
+++ example_test.go
@@ -9,25 +9,25 @@ import (
 func ExampleFind() {
 	// Example with Japanese characters
 	a := "こんにちは世界"
-	la := 10 // Consider the last 10 bytes of the string
+	la := 10
 	index, err := u8p.Find(a, la)
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 	} else {
-		fmt.Printf("Japanese example - Index of UTF-8 lead byte: %d\n", index)
+		fmt.Printf("Japanese example - %s\n", a[:index])
 	}
 
 	// Example with an emoji
-	b := "Hello, 🌍"
-	lb := 8 // Consider the last 8 bytes of the string
+	b := "Hello, 🌍. Hi!"
+	lb := 13
 	index, err = u8p.Find(b, lb)
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 	} else {
-		fmt.Printf("Emoji example - Index of UTF-8 lead byte: %d\n", index)
+		fmt.Printf("Emoji example - %s\n", b[:index])
 	}
 
 	// Output:
-	// Japanese example - Index of UTF-8 lead byte: 9
-	// Emoji example - Index of UTF-8 lead byte: 7
+	// Japanese example - こんに
+	// Emoji example - Hello, 🌍.
 }
